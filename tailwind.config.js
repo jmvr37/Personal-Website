99c8d8d672c2f293b8297fce88163186784cee14
jose <jmvr37@hotmail.com>
2021-10-22 17:06:47 -0700

    change postcss file

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,7 @@ const colors = require('tailwindcss/colors')
 
 module.exports = {
  
-  purge: [
-    './src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
 
   theme: {
     extend: {
