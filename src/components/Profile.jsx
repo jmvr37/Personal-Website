e068c28b3ab3c62c08608ce210c576845285ff39
jose <jmvr37@hotmail.com>
2021-10-23 13:33:01 -0700

    test1

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -25,7 +25,7 @@ const Profile = () =>{
                   <div class="text-white p-3 text-center inline-flex items-center justify-center w-20 h-20 mb-5 shadow-lg rounded-full bg-lightBlue-400 animate-pulse">
                   <img alt="..." src={Keyboard} class="lg: w-16 h-16 blue-400"/>
                   </div>
-                  <h6 class="text-3xl font-mono">Hi, I'm Jose test </h6>
+                  <h6 class="text-3xl font-mono">Hi, I'm Jose </h6>
                   <p class="sm:text-sm mt-2 mb-4 text-blueGray-500 font-light md:text-2xl mt-2 mb-4 lg:">
                   Since the beginning of my journey as a full stack developer 2 years ago, I've done over 10 projects using different technologies, such as Javascript, PHP, and ReactJS.
                   I have worked with talented people to develop digital products for a few non-profit organizations. A self-taught UI/UX designer with a natural curiosity,
